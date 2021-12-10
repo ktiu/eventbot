c98ba58d4a3b1b5b728740eebbcf2563c2c83c9e
ktiu <git@ktiu.net>
2021-12-10 17:00:48 +0100

    serve

---
+++ eventbot.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 3004
+
+app.get('/eventbot', (_, res) => res.send('Eventbot is running.'))
+app.listen(port, () => console.log(`Eventbot listening at http://localhost:${port}`))
+
+app.use('/eventbot/assets', express.static(path.join(__dirname, 'assets')))
